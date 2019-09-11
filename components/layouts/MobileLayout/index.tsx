dbbc53697ac5e46f4c9a57a22ed5c7c6ee0f814a
Аббакумов Евгений <abbevg@gmail.com>
2019-09-11 20:18:29 +0300

    mobile layout

---
+++ components/layouts/MobileLayout/index.tsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+const styles = require('./styles.styl');
+
+export interface MobileLayoutPublicProps {}
+
+export default class MobileLayout extends Component<MobileLayoutPublicProps> {
+    render() {
+        return (
+            <div>
+                <div className={styles.header}>
+                    Cosmetos
+                </div>
+                <div className={styles.content}>
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
