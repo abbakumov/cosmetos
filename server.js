1e11a2bb1bf6a24c61997211e28fc11948b75058
Аббакумов Евгений <abbevg@gmail.com>
2019-10-21 19:28:51 +0300

    api-only dev mode

--- server.js
+++ server.js
@@ -1,38 +1,42 @@
-const Koa = require('koa')
-const next = require('next')
-const Router = require('koa-router')
+const Koa = require('koa');
+const next = require('next');
+const Router = require('koa-router');
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({dev})
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const serverOnly = process.env.COS_SERVER_ONLY === '1';
 
 const makeRouter = require('./api/routes');
 
-app.prepare().then(() => {
-    const server = new Koa()
+const defaultStatucCodeMiddleware = async (ctx, next) => {
+    ctx.res.statusCode = 200;
+    await next();
+}
+
+(async function() {
+    let app, handle;
+
+    const server = new Koa();
 
     // default status code
-    server.use(async (ctx, next) => {
-        ctx.res.statusCode = 200;
-        await next();
-    })
+    server.use(defaultStatucCodeMiddleware);
 
     // Router with API routes
     const router = makeRouter();
 
-    router.get('/some', ctx => {
-        ctx.body = 'Hi!';
-    });
+    if (!serverOnly) {
+        app = next({dev});
+        handle = app.getRequestHandler();
+        await app.prepare();
 
-    // Next routes
-    router.get('*', async ctx => {
-        await handle(ctx.req, ctx.res);
-        ctx.respond = false;
-    })
+        // Next routes
+        router.get('*', async ctx => {
+            await handle(ctx.req, ctx.res);
+            ctx.respond = false;
+        });
+    }
+
     server.use(router.routes());
 
-    server.listen(port, () => {
-        console.log(`> Ready on http://localhost:${port}`)
-    });
-})
+    server.listen(port, () => console.log(`> Ready on http://localhost:${port}`));
+})();
