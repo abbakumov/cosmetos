98242a4d7614b3865dfdb059d44a1264afe5099e
Аббакумов Евгений <abbevg@gmail.com>
2019-11-12 21:36:04 +0300

    getProductColors api

---
+++ api/controllers/getProductColors.js
@@ -0,0 +1,51 @@
+const _ = require('lodash');
+const {
+    Product,
+    ProductColor,
+} = require('../database/models');
+
+module.exports = async function getProductColors(ctx) {
+    const {id} = ctx.params;
+
+    const data = await Product.findByPk(
+        id,
+        {
+            attributes: ['id', 'description'],
+            include: [
+                {
+                    model: ProductColor,
+                    attributes: ['id', 'title'],
+                },
+            ],
+        }
+    );
+
+    const plainData = JSON.parse(JSON.stringify(data));
+    const productColors = plainData.ProductColors;
+
+    const productExtra = {
+        ..._.pick(data, ['id', 'description']),
+        postIds: [], // TODO: now it's always empty
+        bigPicUrl: '', // TODO: this too
+        colorIds: productColors.map(color => color.id),
+    };
+
+    const productColor = productColors.reduce(
+        (acc, color) => ({
+            ...acc,
+            [color.id]: {
+                ...color,
+                picUrl: '', // TODO: and this, yes
+            },
+        }),
+        {}
+    );
+
+    // it's ok for some fields to be empty
+    // if interface will need them, that's easy to do
+
+    ctx.body = {
+        productExtra,
+        productColor,
+    };
+}
