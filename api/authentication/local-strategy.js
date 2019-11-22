19b01c44a1b728772ee637928c3b9020f4093bf3
Аббакумов Евгений <abbevg@gmail.com>
2019-11-23 01:08:01 +0300

    login/password authentication strategy

---
+++ api/authentication/local-strategy.js
@@ -0,0 +1,34 @@
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+
+const {User} = require('../database/models');
+
+const options = {};
+
+const localStrategy = new LocalStrategy(
+    options,
+    async (login, password, done) => {
+        const user = await User.findOne({
+            where: {login}
+        });
+
+        if (!user) {
+            return done(null, false);
+        }
+
+        const passwordComparisonResult = await bcrypt.compare(
+            password,
+            user.passportHash
+        );
+
+        // just to be shure it's boolean value
+        if (passwordComparisonResult === true) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    }
+)
+
+passport.use(localStrategy);
+  
