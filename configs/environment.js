83f9b68fbf4dddb7d277158b1c7dedc456551c71
Аббакумов Евгений <abbevg@gmail.com>
2019-11-22 23:41:02 +0300

    environment helpers and checks

---
+++ configs/environment.js
@@ -0,0 +1,30 @@
+
+const requiredKeys = [
+    'COSMETOS_SECRET',
+    'INSTAGRAM_CLIENT_ID',
+    'INSTAGRAM_CLIENT_SECRET',
+];
+
+const envKeys = requiredKeys.reduce(
+    (acc, key) => ({
+        ...acc,
+        [key]: process.env[key],
+    }),
+    {}
+);
+
+function checkRequiredKeys() {
+    const notProvided = requiredKeys.filter(key => !process.env[key]);
+
+    if (notProvided.length > 0) {
+        const list = notProvided.join(', ');
+        throw new Error(`${list} is not provided`);
+    }
+
+    console.log('All secret keys provided right!');
+};
+
+module.exports = {
+    checkRequiredKeys,
+    envKeys,
+};
