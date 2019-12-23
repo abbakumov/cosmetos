ad9268af39fa55338770f1c5ea2a20b1ac30dec2
Аббакумов Евгений <abbevg@gmail.com>
2019-12-23 18:55:53 +0300

    admin data fetching fix

--- src/helpers/fetchData.ts
+++ src/helpers/fetchData.ts
@@ -3,16 +3,19 @@ import fetch from 'isomorphic-fetch';
 import {ICosPageContext} from '../../types/context';
 
 export default function fetchData<ResponseType>(url: string, options: RequestInit = {}, context?: ICosPageContext) {
-    const isServer = Boolean(typeof process !== 'undefined');
+    const isServer = typeof window === 'undefined';
 
     let fetchOptions: RequestInit = options;
 
     if (isServer && context) {
+        const reqHeaders = context.req.headers || {};
+        const optionsHeaders = options.headers || {};
+    
         const serverHeaders: HeadersInit = {
             // passing all user headers to server
             // very useful for cookies and other stuff
-            ...context.req.headers,
-            ...options.headers,
+            ...reqHeaders,
+            ...optionsHeaders,
         } as HeadersInit;
 
         if (process.env.NODE_ENV === 'production') {
