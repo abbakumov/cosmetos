3a7e59e7cbb7cb68f202983de855aede9de05590
Аббакумов Евгений <abbevg@gmail.com>
2019-12-26 23:43:04 +0300

    admin product color icon fix

--- entities/ProductColor/helpers.js
+++ entities/ProductColor/helpers.js
@@ -1,6 +1,6 @@
 const {cropPic} = require('../../src/helpers/cropPic');
 
-const makeProductColorPicUrl = pic => pic ? `/static/crop/${pic}` : null;
+const makeProductColorPicUrl = pic => pic ? `/static/uploads/${pic}` : null;
 const makeProductColorSmallPicUrl = pic => pic ? `/static/crop/${cropPic(pic, 110)}` : null;
 
 module.exports = {
