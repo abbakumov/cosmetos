5ac5167c55c8d2ce99aef5be1c9a596d4db6e331
Аббакумов Евгений <abbevg@gmail.com>
2020-04-15 22:08:22 +0300

    first tests!

--- api/helpers/postPart.js
+++ api/helpers/postPart.js
@@ -1,4 +1,13 @@
 function getPartProductIds(part, postPartProducts) {
+    // many kinds of incorrect data
+    if (
+        !part
+        || !Array.isArray(part.PostPartProducts)
+        || typeof postPartProducts !== 'object'
+    ) {
+        return [];
+    }
+
     return part.PostPartProducts.map(id => {
         if (postPartProducts[id].Product) {
             return 'a' + postPartProducts[id].Product;
