df3307512140ba1ef8af4e53726a29911fe51a5d
Аббакумов Евгений <abbevg@gmail.com>
2020-04-19 19:58:43 +0300

    avatar fixes

--- entities/Blog/helpers.js
+++ entities/Blog/helpers.js
@@ -1,4 +1,6 @@
-const makeUserAvatarUrl = avatarPicture => `/static/public/user/photo/${avatarPicture}`;
+const {cropPic} = require('../../src/helpers/cropPic');
+
+const makeUserAvatarUrl = avatarPicture => `/static/crop/${cropPic(avatarPicture, 200, 200)}`;
 
 module.exports = {
     makeUserAvatarUrl,
